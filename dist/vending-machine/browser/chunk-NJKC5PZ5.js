348a8283fd3775d8ea4e0b6e281c4eadac948340
Chunghh <chunghh99@gmail.com>
2024-06-16 21:47:30 +0700

    chunghh commit

---
+++ dist/vending-machine/browser/chunk-NJKC5PZ5.js
@@ -0,0 +1,1 @@
+import"./chunk-4L4RMVSS.js";var o=[{path:"",loadComponent:()=>import("./chunk-Z2AU7ZSJ.js").then(t=>t.ReissuePasswordComponent),data:{title:$localize`Cấp lại mật khẩu`}}];export{o as routes};
