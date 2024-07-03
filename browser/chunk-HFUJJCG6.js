f959eda6ae6a7b2e99b3c5ad73cd3fc9ecd74bdf
Chunghh <chunghh99@gmail.com>
2024-07-03 23:09:59 +0700

    chunghh commit code

---
+++ browser/chunk-HFUJJCG6.js
@@ -0,0 +1,1 @@
+import"./chunk-4L4RMVSS.js";var t=[{path:"",loadComponent:()=>import("./chunk-RML7B4QW.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
---
+++ browser/chunk-VN3JL4IJ.js
@@ -0,0 +1,1 @@
+import"./chunk-4L4RMVSS.js";var e=[{path:"",loadComponent:()=>import("./chunk-LZNQW2VP.js").then(t=>t.CreateUserComponent),data:{title:$localize`Khai báo user`}}];export{e as routes};
