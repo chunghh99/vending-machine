ce4c5b5b3777ad67cdc10ea9d4082eeae938703d
Chunghh <chunghh99@gmail.com>
2025-02-19 07:07:22 +0700

    chunghh commit

---
+++ dist/vending-machine/browser/chunk-BKZFVQBW.js
@@ -0,0 +1,1 @@
+var n={apiUrl:"https://vending.koyeb.app"};export{n as a};
---
+++ dist/vending-machine/browser/chunk-BRUOMGQB.js
@@ -0,0 +1,1 @@
+import"./chunk-4L4RMVSS.js";var o=[{path:"",loadComponent:()=>import("./chunk-MNUMQUBO.js").then(t=>t.ReissuePasswordComponent),data:{title:$localize`Cấp lại mật khẩu`}}];export{o as routes};
